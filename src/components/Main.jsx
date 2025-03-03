125a4932b24ca00e293737859874069b3bdb3a22
Logan <lmschwappach@gmail.com>
2025-03-03 16:38:03 -0600

    Lesson 1 finished

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main () {
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Blah</li>
+                <li>Blahh</li>
+                <li>Blahhh</li>
+                <li>Blahhhh</li>
+                <li>Blahhhhh</li>
+            </ul>
+        </main>
+    )
+}
