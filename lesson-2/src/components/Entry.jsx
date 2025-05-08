bd6c7a23bd13ab06c1bdd3c5e88b8975e476d833
Logan <lmschwappach@gmail.com>
2025-05-08 13:12:19 -0700

    Add lesson-2 initial setup

---
+++ lesson-2/src/components/Entry.jsx
@@ -0,0 +1,13 @@
+export default function Entry(props) {
+    return (
+        <article>
+            <img src="" alt="" />
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    
+                </div>
+            </div>
+        </article>
+    )
+}
---
+++ lesson-2/src/mainContent.jsx
@@ -0,0 +1,14 @@
+export default function MainContent() {
+    return (
+        <main> 
+            <h1>Why i'm excited to do</h1>
+            <ol>
+                <li>Website</li>
+                <li>Blah</li>
+                <li>blah</li>
+                <li>blahh</li>
+                <li>blahhh</li>
+            </ol>
+        </main>
+    )
+}
